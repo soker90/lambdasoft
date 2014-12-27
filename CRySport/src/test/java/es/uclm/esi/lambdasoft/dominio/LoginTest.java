820b88cef535d919aebc6936d919e8514fc480fb
jmsierra91 <jmsierra91@gmail.com>
2014-12-27 20:15:45 +0000

    Version que funciona bien que te cagas energy

--- CRySport/src/test/java/es/uclm/esi/lambdasoft/dominio/LoginTest.java
+++ CRySport/src/test/java/es/uclm/esi/lambdasoft/dominio/LoginTest.java
@@ -1,8 +1,5 @@
 package es.uclm.esi.lambdasoft.dominio;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import junit.framework.TestCase;
 import es.uclm.esi.lambdasoft.dominio.Usuario;
 
@@ -11,51 +8,38 @@ public class LoginTest extends TestCase
  
 	public void testLoginCorrecto()
 	{
- 		
-		try{
-			/* Setup */
-			Usuario u = new Usuario("g02.03","g02.03");
-			 
-			/* Ejecución del esceneario */
-			boolean resultado = u.select();
-			 
-			/* Oráculo */
-			assertTrue(resultado);		
-		}catch (Exception e){
-			fail("Ha surgido un problema: "+e.toString());
-		}		
+ 		/* Setup */
+		Usuario u = new Usuario("g02.03","g02.03");
+		 
+		/* Ejecución del esceneario */
+		boolean resultado = u.select();
+		 
+		/* Oráculo */
+		assertTrue(resultado);
 	}
  
  
 	public void testLoginIncorrecto()
 	{
-		try{
-			/* Setup */
-			Usuario u = new Usuario("No existe tal usuario","Con esta contraseña");
-	 
-			/* Ejecución del esceneario */
-			boolean resultado = u.select();
-	 
-			/* Oráculo */
-			assertFalse(resultado);	
-		}catch (Exception e){
-			fail("Ha surgido un problema: "+e.toString());
-		}	
+		/* Setup */
+		Usuario u = new Usuario("No existe tal usuario","Con esta contraseña");
+ 
+		/* Ejecución del esceneario */
+		boolean resultado = u.select();
+ 
+		/* Oráculo */
+		assertFalse(resultado);
 	}
  
 	public void testSQLInjection()
 	{
-		try{
-			/* Setup */
-			Usuario u = new Usuario("'g02.03","'g02.03");
-	 
-			/* Ejecución del esceneario */
-			boolean resultado = u.select();
-	 
-			/* Oráculo */
-			assertTrue(resultado);
-		}catch (Exception e){
-			fail("Ha surgido un problema: "+e.toString());
-		}	
+		/* Setup */
+		Usuario u = new Usuario("'g02.03","'g02.03");
+ 
+		/* Ejecución del esceneario */
+		boolean resultado = u.select();
+ 
+		/* Oráculo */
+		assertTrue(resultado);
 	}
 }
